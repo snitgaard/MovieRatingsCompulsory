d5ebfaa0eb505d639d659ca3cce34aec72847872
dreamy <31621059+dreamyy1@users.noreply.github.com>
2020-10-01 13:26:45 +0200

    removed useless interface things

--- MovieRatingsService/Core/Interfaces/IMovieRatingsRepository.cs
+++ MovieRatingsService/Core/Interfaces/IMovieRatingsRepository.cs
@@ -6,17 +6,5 @@ namespace MovieRatingsApplication.Core.Interfaces
     public interface IMovieRatingsRepository
     {
         IList<MovieRating> GetAllMovieRatings();
-        int GetNumberOfReviewsFromReviewer(int reviewer);
-        double GetAverageRateFromReviewer(int reviewer);
-        int GetNumberOfRatesByReviewer(int reviewer, int rate);
-        IList<MovieRating> GetNumberOfReviews();
-        double GetAverageRateOfMovie(int movie);
-        int GetNumberOfRates(int movie, int rate);
-        List<int> GetMoviesWithHighestNumberOfTopRates();
-        List<int> GetMostProductiveReviewers();
-        List<int> GetTopRatedMovies(int amount);
-        List<int> GetTopMoviesByReviewer(int reviewer);
-        List<int> GetReviewersByMovie(int movie);
-
     }
 }
